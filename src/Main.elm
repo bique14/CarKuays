fc807ffcdf304a1270273392a9606678dca86117
bbibbibiq <peerasorn@hot-now.com>
2019-08-25 17:40:27 +0700

    🌱Initial project

---
+++ src/Main.elm
@@ -0,0 +1,37 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+
+
+type Msg
+    = None
+
+
+type alias Model =
+    {}
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
+
+
+init : Model
+init =
+    {}
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text "Hello world!" ]
